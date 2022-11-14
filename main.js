21f3d3d87d02793eb25e6485b27b3404fb05a05d
vlisak <vlisakml@gmail.com>
2022-11-14 17:33:11 +0100

    Vyhledávání done

--- main.js
+++ main.js
@@ -2,13 +2,15 @@ const hamburger = document.querySelector(".main-nav__hamburger"),
       itemsDiv = document.querySelector(".main-nav__items"),
       text = hamburger.lastElementChild,
       items = document.querySelectorAll(".main-nav__items > a"),
-      importantInfo = document.querySelector(".important-info");
+      importantInfo = document.querySelector(".important-info"),
+      activateSearch = document.querySelector(".search-button"),
+      searchBar = document.querySelector(".search-bar");
 
 if(localStorage.getItem("visited") == "true") {
     importantInfo.style.display = "none";
 }
 
-hamburger.addEventListener("click", () => {
+hamburger.onclick = () => {
     hamburger.classList.toggle("active");
     itemsDiv.classList.toggle("active");
 
@@ -16,7 +18,7 @@ hamburger.addEventListener("click", () => {
         text.textContent = "Zavřít";
     else
         text.textContent = "Menu";
-});
+};
 
 items.forEach(item => {
     item.addEventListener("click", () => {
@@ -27,7 +29,28 @@ items.forEach(item => {
     });
 });
 
-importantInfo.addEventListener("click", () => {
+importantInfo.onclick = () => {
     importantInfo.style.display = "none";
     localStorage.setItem("visited", true);
+};
+
+activateSearch.onclick = () => {
+    searchBar.classList.toggle("active");
+    activateSearch.querySelector("i").textContent = 
+    searchBar.classList.contains("active") ? "close" : "search";
+}
+
+searchBar.querySelectorAll("button").forEach((item, index) => {
+        if(index == 0) {
+            item.addEventListener("click", (e)=> {
+                window.location = "vyhledavani.htm";
+                e.preventDefault();
+            });
+        } else {
+            item.addEventListener("click", (e)=> {
+                activateSearch.querySelector("i").textContent = "search";
+                searchBar.classList.remove("active");
+                e.preventDefault();
+            });
+        }
 });
